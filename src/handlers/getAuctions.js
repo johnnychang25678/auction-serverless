6359c40e83cd4af4092cbf66778834264b047bac
Johnny Chang <zjps3407@gmail.com>
2021-03-27 21:34:11 +0800

    add feature to get auctions with status query string

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,31 +1,47 @@
 import AWS from 'aws-sdk'
 import commonMiddleware from '../lib/commonMiddleware'
 import createError from 'http-errors'
+import validator from '@middy/validator'
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 async function getAuctions(event, context) {
   let auctions
+  const {status} = event.queryStringParameters // like req.query
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status' // 'status' is a reserved word
+    }
+  }
 
   try {
-    const result = await dynamoDb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
-    }).promise()
+    const result = await dynamoDb.query(params).promise()
     auctions = result.Items
+    return {
+    statusCode: 200, 
+    // lamda body needs to be string
+    body: JSON.stringify(auctions),
+  }
   } catch (err) {
     console.error(err)
     throw new createError.InternalServerError(err)
   }
-
-
-  return {
-    statusCode: 200, 
-    // lamda body needs to be string
-    body: JSON.stringify(auctions),
-  };
+  
 }
 
 export const handler = commonMiddleware(getAuctions)
+  .use(validator({
+    inputSchema: getAuctionsSchema,
+    useDefaults: true
+  }))
 
 
 
