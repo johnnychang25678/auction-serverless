1c57606bf19a015c86bb55912f3f04593fadb434
Johnny Chang <zjps3407@gmail.com>
2021-03-29 22:27:37 +0800

    add email notification to seller and bidder feature

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,8 +1,9 @@
 import AWS from 'aws-sdk'
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const sqs = new AWS.SQS()
 
-export async function closeAuction(auction) {
+export async function closeAuction(auction) { // change status from OPEN to CLOSE
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: {id: auction.id},
@@ -15,6 +16,30 @@ export async function closeAuction(auction) {
     }
   }
 
-  const result = await dynamoDb.update(params).promise()
-  return result
+  await dynamoDb.update(params).promise()
+
+  const {title, seller, highestBid} = auction
+  const {amount, bidder} = highestBid
+
+  const notfifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'Your item has been sold!',
+      recipient: seller,
+      body: `Woohoo! Your item "${title}" has been sold for $${amount}`
+    })
+  }).promise()
+
+  const notfifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'You won an auction',
+      recipient: bidder,
+      body: `What a great deal! You got yourself a "${title}" for $${amount}`
+    })
+  }).promise()
+
+  return Promise.all([notfifySeller, notfifyBidder])
+
+  
 }
