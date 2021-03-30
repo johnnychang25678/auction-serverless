a0c64aabdc384c8c4271d93de8d4d894fc6619c4
Johnny Chang <zjps3407@gmail.com>
2021-03-30 14:54:21 +0800

    add validation to uploadPicture

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -4,33 +4,45 @@ import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl'
 import middy from '@middy/core'
 import httpErrorHander from '@middy/http-error-handler'
 import createError from 'http-errors'
+import validator from '@middy/validator'
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema'
 
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters
+  const { email } = event.requestContext.authorizer
+  const auction = await getAuctionById(id)
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden('You are not the seller for this auction!')
+  }
+  
+  // need to strip off some charaters to store in S3`
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
+  const buffer = Buffer.from(base64, 'base64')
+
+  if (buffer.toString('base64') !== base64) {
+    throw new createError.BadRequest('Invalid base64 string for image')
+  }
+
+  let updatedAuction
+  
   try {
-    const { id } = event.pathParameters
-    const { email } = event.requestContext.authorizer
-    const auction = await getAuctionById(id)
-
-    if (auction.seller !== email) {
-      throw new createError.Forbidden('You are not the seller for this auction!')
-    }
-    
-    // need to strip off some charaters to store in S3`
-    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
-    const buffer = Buffer.from(base64, 'base64')
-
     const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer)
-    const updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(updatedAuction)
-    }
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl) 
   } catch (err) {
     console.error(err)
     throw new createError.InternalServerError(err)
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction)
+  }
+
 }
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHander())
+  .use(validator({
+    inputSchema: uploadAuctionPictureSchema
+  }))
